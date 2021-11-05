4df63776eec94c5616c49b2b47ec4075f60d31d4
troy <hunter007nz@gmail.com>
2021-11-06 00:47:55 +1300

    added custom default likes to post controller

--- first-strapi-app/api/post/controllers/post.js
+++ first-strapi-app/api/post/controllers/post.js
@@ -5,4 +5,30 @@
  * to customize this controller
  */
 
-module.exports = {};
+ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+ module.exports = {
+   /**
+    * Create a record.
+    *
+    * @return {Object}
+    */
+
+   async create(ctx) {
+     let entity;
+     if (ctx.is('multipart')) {
+       const { data, files } = parseMultipartData(ctx);
+
+       if (!data || !data.description || !data.title) {
+         ctx.throw(400, "please add some data")
+       }
+       if (!files || !files.image) {
+        ctx.throw(400, "please add a file")
+      }
+       entity = await strapi.services.post.create({...data, likes: 0,}, { files,  });
+     } else {
+       ctx.throw(400, 'You must submit a multipart req')
+     }
+     return sanitizeEntity(entity, { model: strapi.models.post });
+   },
+ };
